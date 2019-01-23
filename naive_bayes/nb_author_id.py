bab4e321728bfb10675659acd9bbb0e44908e40a
YunpengCai <caiyunpeng1989@gmail.com>
2019-01-24 00:08:33 +0100

    Machine learning with different models
    Naive_bayes, support vector machine, decision_tree and random forest
    used.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,7 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -25,9 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
 
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
 
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels
+  
+    clf.fit(features_train, labels_train)
+    ### use the trained classifier to predict labels for the test features
+    pred = clf.predict(features_test)
+
+    
+    ### calculate and return the accuracy on the test data
+   
+    accuracy = accuracy_score(labels_test, pred)
+    return accuracy
 #########################################################
 
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,19 +12,24 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+def svmaccuracy(features_train, features_test, labels_train, labels_test):
 
 
 
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+    t0 = time()
+    clf=SVC(kernel='linear")
+    clf.fit(features_train, labels_train)
+    print "training time:", round(time()-t0, 3), "s"        
+    
+    t0 = time()
+    pred=clf.predict(features_test)
+    print "prediction time:", round(time()-t0, 3), "s"
+    score=accuracy_score(labels_test,pred)
+    print score
